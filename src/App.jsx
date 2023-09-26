4535d7bbf72ae6061ff8975182f674900d460bd5
Hester910 <hadassa.ester@ufrpe.br>
2023-09-26 18:21:51 -0300

    teste das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <div className='App'>
+      <nav id='navbar'>
+        <h2>
+          <Link to='/'>MoviesLib</Link>
+        </h2>
+        <Link to='/movies/1'>Movie</Link>
+        <Link to='/search'>Search</Link>
+      </nav>
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
   )
 }
